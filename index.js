80db6854b9589f7eb36382566ee8023e178e6dab
Sayan Bera <berasayan2002.raja@gmail.com>
2025-06-02 04:03:05 +0530

    Fix: removed NODE_ENV check to allow server to run in production

--- index.js
+++ index.js
@@ -43,11 +43,10 @@ mongoose.connect(databaseURL, {
 .then(() => console.log("DB Connection successful"))
 .catch(err => console.log(`Some error occurred while connecting to database: ${err}`));
 
-if (process.env.NODE_ENV !== "production") {
-    server.listen(port, () => {
-        console.log(`Server is running at http://localhost:${port}`);
-    });
-}
+server.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
+
 export default server;
 
 setupSocket(server);
