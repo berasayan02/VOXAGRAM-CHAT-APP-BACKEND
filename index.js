ccdf8ac31dc17e76318192e7ea7d65e9d91aacdf
Sayan Bera <berasayan2002.raja@gmail.com>
2025-06-02 03:48:13 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,53 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import http from "http";
+import cookieParser from "cookie-parser";
+import mongoose from "mongoose";
+import authRouter from "./routes/AuthRoutes.js";
+import contactsRoutes from "./routes/ContactRoutes.js";
+import setupSocket from "./socket.js";
+import messageRoutes from "./routes/MessagesRoutes.js";
+import channelRoutes from "./routes/ChannelRoutes.js";
+
+
+dotenv.config();
+const app = express();
+const port = process.env.PORT || 3001;
+const databaseURL = process.env.DATABASE_URL;
+const server = http.createServer(app);
+
+
+app.use(
+    cors({
+        origin: process.env.ORIGIN,
+        methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+        credentials: true
+    })
+);
+
+app.use('/uploads/profiles', express.static('uploads/profiles'));
+app.use('/uploads/files', express.static('uploads/files'));
+
+app.use(cookieParser());
+app.use(express.json());
+app.use("/api/auth", authRouter);
+app.use("/api/contacts", contactsRoutes);
+app.use("/api/messages", messageRoutes);
+app.use("/api/channel", channelRoutes);
+
+
+mongoose.connect(databaseURL, {
+    dbName: "VOXAGRAM"
+})
+.then(() => console.log("DB Connection successful"))
+.catch(err => console.log(`Some error occurred while connecting to database: ${err}`));
+
+if (process.env.NODE_ENV !== "production") {
+    server.listen(port, () => {
+        console.log(`Server is running at http://localhost:${port}`);
+    });
+}
+export default server;
+
+setupSocket(server);
